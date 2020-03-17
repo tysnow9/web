03868529fb7ae06cfdc68270057a309aa72db048
Tyler Wood <tysnow@gmail.com>
2020-03-17 15:54:42 -0700

    Added react app (hopefully without node_models)

---
+++ test-1/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Some kind of introduction here...</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
